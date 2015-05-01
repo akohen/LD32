ab3552be84ec55fa258b293638420e9e0f28bfa6
Alexandre KOHEN <alexandre@kohen.fr>
2015-05-01 12:54:07 +0200

    Smaller arrows, arrows group

--- ld32.js
+++ ld32.js
@@ -1,13 +1,21 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
+var arrows;
 
 function preload () {
-  game.load.image('logo', 'phaser.png');
+  game.load.image('arrow', 'assets/arrow_up.png');
 }
 
 function create () {
-  var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-  logo.anchor.setTo(0.5, 0.5);
+  arrows = game.add.group();
+  arrows.physicsEnabled = true
+  arrows.enableBody = true;
+  arrows.physicsBodyType = Phaser.Physics.ARCADE;
+
+  arrows.create(400, 300, 'arrow');
+  arrows.create(500, 300, 'arrow');
+  arrows.create(600, 300, 'arrow');
+
+  arrows.setAll('body.velocity.x', -25);
   console.log('test');
 }
 
