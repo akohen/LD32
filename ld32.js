d17baf6f7ea12a8aee3667fd09a3c5d234c0ad56
Alexandre KOHEN <alexandre@kohen.fr>
2015-05-01 13:01:21 +0200

    Detect overlap

--- ld32.js
+++ ld32.js
@@ -1,9 +1,13 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
 var arrows;
+var player;
 
 function preload () {
   game.load.image('arrow', 'assets/arrow_up.png');
+  game.load.image('arrowDown', 'assets/arrow_down.png');
 }
+
 
 function create () {
   arrows = game.add.group();
@@ -15,10 +19,17 @@ function create () {
   arrows.create(500, 300, 'arrow');
   arrows.create(600, 300, 'arrow');
 
-  arrows.setAll('body.velocity.x', -25);
-  console.log('test');
+  arrows.setAll('body.velocity.x', -100);
+
+  player = game.add.sprite(100,250, 'arrowDown');
+  game.physics.enable(player, Phaser.Physics.ARCADE);
 }
 
-function update() {
 
+function update() {
+  game.physics.arcade.overlap(player, arrows, collisionHandler, null, this);
+}
+
+function collisionHandler() {
+  console.log('test');
 }
