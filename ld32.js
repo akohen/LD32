5ac357e71ac244c2d10dce84dae2ae72ee96f37f
MaReineLaFee <wsflo@hotmail.com>
2015-05-01 13:56:33 +0200

    Arrow appears at regular time

--- ld32.js
+++ ld32.js
@@ -2,6 +2,8 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 var arrows;
 var player;
+var apparitionTime;
+var speed = -100;
 
 function preload () {
   game.load.image('arrow', 'assets/arrow_up.png');
@@ -19,17 +21,28 @@ function create () {
   arrows.create(500, 300, 'arrow');
   arrows.create(600, 300, 'arrow');
 
-  arrows.setAll('body.velocity.x', -100);
+  arrows.setAll('body.velocity.x', speed);
 
   player = game.add.sprite(100,250, 'arrowDown');
   game.physics.enable(player, Phaser.Physics.ARCADE);
+
+  apparitionTime = 10;
+
 }
 
 
 function update() {
   game.physics.arcade.overlap(player, arrows, collisionHandler, null, this);
+
+  if (apparitionTime == 0) {
+  	arrows.create(750, 300, 'arrow').body.velocity.x = speed;
+  	apparitionTime = 100;
+  }
+  
+  apparitionTime-- ;
+
 }
 
-function collisionHandler() {
+function collisionHandler(player, arrow) {
   console.log('test');
 }
