500214c6b1fba1ef05fb4dedc9fcfd55663bf733
Alexandre KOHEN <alexandre@kohen.fr>
2015-05-01 12:34:36 +0200

    File structure

---
+++ ld32.js
@@ -0,0 +1,16 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+
+function preload () {
+  game.load.image('logo', 'phaser.png');
+}
+
+function create () {
+  var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+  logo.anchor.setTo(0.5, 0.5);
+  console.log('test');
+}
+
+function update() {
+
+}
