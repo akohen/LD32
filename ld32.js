3d2bd54c9b4a2d046f46bb46146c39df7cb163fa
Alexandre KOHEN <alexandre@kohen.fr>
2015-05-03 20:47:34 +0200

    New backgrounds, remedial exam

--- ld32.js
+++ ld32.js
@@ -16,6 +16,7 @@ var menuState = {
     game.load.image('screenPunition', 'assets/screenPunition.png');
 
     game.load.image('gameOver', 'assets/screenGameOver.png');
+    game.load.image('end', 'assets/screenEnd.png');
 
     game.load.audio('pickup', 'assets/pickup.wav');
     game.load.audio('soundNote', 'assets/soundNote.mp3');
@@ -49,7 +50,7 @@ var gameOverState = {
     game.add.sprite(0,0, 'gameOver');
     game.add.text(250, 450, 'Press any key', { font: "italic 26px Arial", fill: '#000000'});
     game.input.keyboard.onDownCallback = function(e) {
-      game.state.start('game');
+      game.state.start('menu');
       game.input.keyboard.onDownCallback = null;
     }
   },
@@ -58,22 +59,22 @@ var gameOverState = {
 
 var winState = {
   create: function () {
-    game.add.sprite(0,0, 'background');
+    game.add.sprite(0,0, 'end');
     cursors = game.input.keyboard.createCursorKeys();
-    game.add.text(180, 330, 'Geography',  { font: "40px Arial", fill: '#000000'});
-    game.add.text(400, 330, levels['geography'].grade,  { font: "40px Arial", fill: '#000000'});
+    game.add.text(80, 310, 'Geography',  { font: "40px Arial", fill: '#000000'});
+    game.add.text(360, 310, levels['geography'].grade,  { font: "40px Arial", fill: '#000000'});
 
-    game.add.text(180, 370, 'Language',  { font: "40px Arial", fill: '#000000'});
-    game.add.text(400, 370, levels['language'].grade,  { font: "40px Arial", fill: '#000000'});
+    game.add.text(80, 360, 'Language',  { font: "40px Arial", fill: '#000000'});
+    game.add.text(360, 360, levels['language'].grade,  { font: "40px Arial", fill: '#000000'});
 
-    game.add.text(180, 410, 'Science',  { font: "40px Arial", fill: '#000000'});
-    game.add.text(400, 410, levels['science'].grade,  { font: "40px Arial", fill: '#000000'});
+    game.add.text(80, 410, 'Science',  { font: "40px Arial", fill: '#000000'});
+    game.add.text(360, 410, levels['science'].grade,  { font: "40px Arial", fill: '#000000'});
 
-    game.add.text(180, 450, 'Sport',  { font: "40px Arial", fill: '#000000'});
-    game.add.text(400, 450, levels['sport'].grade,  { font: "40px Arial", fill: '#000000'});
+    game.add.text(80, 460, 'Sport',  { font: "40px Arial", fill: '#000000'});
+    game.add.text(360, 460, levels['sport'].grade,  { font: "40px Arial", fill: '#000000'});
 
     game.input.keyboard.onDownCallback = function(e) {
-      game.state.start('game');
+      game.state.start('menu');
       game.input.keyboard.onDownCallback = null;
     }
   },
@@ -86,7 +87,7 @@ var gameState = {
     background = game.add.image(0,0, 'screenResult');
     background.loadTexture('screenScience');
 
-    resultDisplay = game.add.text(350, 350, '',  { font: "40px Arial", fill: '#222222'});
+    resultDisplay = game.add.text(375, 350, '',  { font: "40px Arial", fill: '#222222'});
     gradeDisplay = game.add.text(520, 340, '',  { font: "56px Arial", fill: '#ff2222'});
 
     arrows = game.add.group();
@@ -98,7 +99,7 @@ var gameState = {
     game.physics.enable(player, Phaser.Physics.ARCADE);
     player.anchor.x = 0.5;
     player.anchor.y = 0.5;
-    player.body.width = 2;
+    player.body.width = 5;
     player.body.height = 120;
 
     cursors = game.input.keyboard.createCursorKeys();
@@ -107,11 +108,11 @@ var gameState = {
     resultSound = game.add.audio('soundNote');
 
     levels = {
-      geography : { speed : 200, speedVar : 50, spawnTime : 50, spawnTimeVar : 0, duration : 2,   background : 'screenGeography', music : game.add.audio('soundGeography'), next : 'language'},
-      language :  { speed : 200, speedVar : 50, spawnTime : 50, spawnTimeVar : 30, duration : 4,  background : 'screenLanguage',  music : game.add.audio('soundLanguage'), next : 'science'},
-      science :   { speed : 400, speedVar : 50, spawnTime : 10, spawnTimeVar : 30, duration : 8,  background : 'screenScience',   music : game.add.audio('soundScience'), next : 'sport'},
-      sport :     { speed : 400, speedVar : 50, spawnTime : 10, spawnTimeVar : 30, duration : 8,  background : 'screenSport',     music : game.add.audio('soundSport'), next : 'win'},
-      punition :  { speed : 100, speedVar : 0, spawnTime : 100, spawnTimeVar : 0, duration : 2,   background : 'screenPunition',  music : game.add.audio('soundPunition'), next : 'punition'},
+      geography : { name: 'geography',  speed : 200, speedVar : 0, spawnTime : 30, spawnTimeVar : 15, duration : 20,    background : 'screenGeography', music : game.add.audio('soundGeography'), next : 'language'},
+      language :  { name: 'language',   speed : 300, speedVar : 25, spawnTime : 10, spawnTimeVar : 40, duration : 30,   background : 'screenLanguage',  music : game.add.audio('soundLanguage'), next : 'science'},
+      science :   { name: 'science',    speed : 470, speedVar : 0, spawnTime : 3, spawnTimeVar : 40, duration : 40,     background : 'screenScience',   music : game.add.audio('soundScience'), next : 'sport'},
+      sport :     { name: 'sport',      speed : 350, speedVar : 100, spawnTime : 20, spawnTimeVar : 10, duration : 40,  background : 'screenSport',     music : game.add.audio('soundSport'), next : 'win'},
+      punition :  { name: 'punition',   speed : 150, speedVar : 0, spawnTime : 150, spawnTimeVar : 0, duration : 10,    background : 'screenPunition',  music : game.add.audio('soundPunition'), next : 'punition'},
     };
 
     this.loadLevel('geography');
@@ -154,7 +155,7 @@ var gameState = {
     level.music.stop();
     player.kill();
     background.loadTexture('screenResult');
-    resultDisplay.text = 'Résultat ';
+    resultDisplay.text = 'Grade';
     resultDisplay.revive();
     resultSound.play();
     game.time.events.add(Phaser.Timer.SECOND * 3, this.showGrade, this);
@@ -170,7 +171,13 @@ var gameState = {
 
   loadNextLevel: function() {
     if( result < 0.5 ) { // Lose
-      game.state.start('gameOver');
+      background.loadTexture('gameOver');
+      resultDisplay.kill();
+      gradeDisplay.kill();
+      game.input.keyboard.onDownCallback = function(e) {
+        game.input.keyboard.onDownCallback = null;
+        gameState.loadLevel(level.name);
+      }
     } else if( result >= 0.80 && level.next != 'punition') { // Punition
       this.loadLevel('punition');
     } else if( level.next == 'win' ) { // Win
@@ -229,7 +236,7 @@ var gameState = {
         key = 'Right';
       }
       if( key != '' ) {
-        cooldown = 10;
+        cooldown = 8;
       }
     } else {
       cooldown--;
